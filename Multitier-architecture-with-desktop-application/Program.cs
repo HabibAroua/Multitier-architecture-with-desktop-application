e70834614f4b2727985ddb4d0664d04650f0cf9a
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-07-13 22:38:30 +0100

    create new project

---
+++ Multitier-architecture-with-desktop-application/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multitier_architecture_with_desktop_application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
