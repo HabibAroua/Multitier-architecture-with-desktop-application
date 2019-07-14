dfe0394dd3c4853bd79d95f476e479fc99d82343
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-07-14 21:18:38 +0100

    before test

--- Multitier-architecture-with-desktop-application/Program.cs
+++ Multitier-architecture-with-desktop-application/Program.cs
@@ -7,11 +7,12 @@ namespace Multitier_architecture_with_desktop_application
     {
         static void Main(string[] args)
         {
-            GetRequest("http://www.google.com");
+            //GetRequest("http://www.google.com");
+            GetRequestHeades("http://www.google.com");
             Console.ReadLine();
         }
 
-        async static void GetRequest(String url)
+        async static void GetRequest(string url)
         {
             try
             {
@@ -28,6 +29,31 @@ namespace Multitier_architecture_with_desktop_application
                 }
             }
             catch(HttpRequestException Ex)
+            {
+                Console.WriteLine("Error : " + Ex.Message);
+                Console.ReadLine();
+                Console.WriteLine("Press in any key for exit .....");
+                Console.ReadLine();
+            }
+        }
+
+        async static void GetRequestHeades(string url)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    using (HttpResponseMessage response = await client.GetAsync(url))
+                    {
+                        using (HttpContent httpContent = response.Content)
+                        {
+                            System.Net.Http.Headers.HttpContentHeaders headers = httpContent.Headers;
+
+                        }
+                    }
+                }
+            }
+            catch (HttpRequestException Ex)
             {
                 Console.WriteLine("Error : " + Ex.Message);
                 Console.ReadLine();
